90764f4e7c00de240d914c24c4345eab3933ee96
Shamel Sameer <47436712+shamelsameer@users.noreply.github.com>
2020-11-09 10:37:01 -0600

    Add files via upload

---
+++  minimax-ai-no_optimizations.py
@@ -0,0 +1,148 @@
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = 0
+
+
+def initial_state():
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    count = 0
+    for x in board:
+        for y in x:
+            if y == EMPTY:
+                count+=1
+    if count%2==1:
+        return X
+    else:
+        return O
+
+
+def actions(board):
+    acts = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                acts.add((i,j))
+    return acts
+
+
+def result(board, action):
+    if action in actions(board):
+        (i, j) = action
+        current_player = player(board)
+        new_board = copy.deepcopy(board)
+        new_board[i][j] = current_player
+        return new_board
+    else:
+        raise Exception
+
+
+def winner(board):
+    xs = 0
+    ys = 0
+    xother=0
+    oother =0
+    for row in board:
+        xs = row.count(X)
+        ys = row.count(O)
+        if xs == 3:
+            return X
+        if ys == 3:
+            return O
+    for x in range(3):
+        xother = 0
+        oother =0
+        for y in range(3):
+            if board[y][x]==X:
+                xother+=1
+            if board[y][x]==O:
+                oother+=1
+        if xother ==3:
+            return X
+        if oother ==3:
+            return O
+    if (board[0][0]== X and board[1][1]==X and board[2][2] ==X):
+        return X
+    if (board[0][2]==X and board[1][1]==X and board[2][0]==X):
+        return X
+    if (board[0][0]== O and board[1][1]==O and board[2][2] ==O):
+        return O
+    if (board[0][2]==O and board[1][1]==O and board[2][0]==O):
+        return O
+    return None
+
+def terminal(board):
+    if winner(board)==X or winner(board)==O:
+        return True
+    for x in range(3):
+        for y in range(3):
+            if board[x][y]==EMPTY:
+                return False
+    return True
+    
+
+
+def utility(board):
+    if winner(board)==X:
+        return 1
+    if winner(board)==O:
+        return -1
+    else:
+        return 0
+    
+    
+
+
+def minimax(board):
+    if terminal(board):
+        return None
+    return helper(board)
+                
+
+
+def helper(board):
+    smallaction = None
+    largeaction = None
+    smallest = 5000
+    largest = -5000
+    new_board = None
+    for x in actions(board):
+        new_board = result(board,x)
+        if player(board)==X:
+            if minO(new_board)>largest:
+                largeaction = x
+                largest=minO(new_board)
+        if player(board)==O:
+            if maxX(new_board)<smallest:
+                smallaction = x
+                smallest = maxX(new_board)
+    if player(board)==X:
+        return largeaction
+    else:
+        return smallaction
+
+
+def maxX(board):
+    largevalue = -1000
+    if terminal(board):
+        return utility(board)
+    for x in actions(board):
+        new_board = result(board,x)
+        largevalue = max(largevalue, minO(new_board))
+    return largevalue
+
+def minO(board):
+    smallvalue = 1000
+    if terminal(board):
+        return utility(board)
+    for x in actions(board):
+        new_board = result(board,x)
+        smallvalue = min(smallvalue, maxX(new_board))
+    return smallvalue
---
+++ pygame-tictactoe.py
@@ -0,0 +1,70 @@
+import pygame
+from x-o_grid import Grid
+import minimax-ai-no_optimizations
+
+surface = pygame.display.set_mode((600,600))
+pygame.display.set_caption("Tic-Tac-Toe")
+
+grid = Grid()
+
+player = "X"
+
+
+    
+def game_loop():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0] == 1:
+                        x=pygame.mouse.get_pos()[0]//200
+                        y=pygame.mouse.get_pos()[1]//200
+                        grid.play(x,y, player)
+                        if ai.terminal(grid.matrix):
+                            grid.draw(surface)
+                        else:
+                            act =  minimax-ai-no_optimizations.minimax(grid.matrix)
+                            grid.matrix =  minimax-ai-no_optimizations.result(grid.matrix,act)
+                            
+        surface.fill((0,0,0))
+
+        grid.draw(surface)
+        pygame.display.flip()
+
+
+
+def intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        surface.fill((200,200,200))
+        pygame.font.init() 
+        myfont = pygame.font.SysFont('Comic Sans MS', 100)
+        textsurface3 = myfont.render('Tic-Tac-Toe', False, (200, 0, 200))
+        surface.blit(textsurface3,(125,225))
+        
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if 250+100 > mouse[0] > 250 and 350+50 > mouse[1] > 350:
+            pygame.draw.rect(surface, (100,100,100), (250,350,100,50))
+            if click[0] == 1:
+                game_loop()
+        else:
+            pygame.draw.rect(surface, (0,0,0), (250,350,100,50))
+        
+        pygame.font.init() 
+        myfont = pygame.font.SysFont('Comic Sans MS', 40)
+        textsurface4 = myfont.render('Play', False, (200, 200, 200))
+        surface.blit(textsurface4,(270,365))
+        pygame.display.update()
+        pygame.display.flip()
+
+intro()
+game_loop()
